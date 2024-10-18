b39b173f7c17db004b8cad5f7e62341cb5f38b28
Ansh Chaube <afc6440@psu.edu>
2024-10-18 16:55:02 -0400

    nek only build, oct 24

---
+++ frontier/frontier-cardinal-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+module purge
+module load PrgEnv-gnu
+module load Core/24.07
+
+module load ums/default ums002/default cray-python
+pip install --user pyyaml jinja2 packaging
+
+module load gcc/12.2.0
+module load craype-accel-amd-gfx90a
+module load cray-mpich/8.1.27
+module load rocm
+module unload cray-libsci
+module load cmake/3.27
+module list
+
+export OCCA_HIP_ENABLED=1
+export ENABLE_OPENMC=false
+
+export CARDINAL_DIR=$(realpath -P /lustre/orion/scratch/achaube/cfd202/cardinal)
+export NEKRS_HOME=$CARDINAL_DIR/install
+echo "Default MOOSE python path:${PYTHONPATH}"
+export PYTHONPATH=$CARDINAL_DIR/contrib/moose/python
+export MOOSE_DIR=$CARDINAL_DIR/contrib/moose
+export LIBMESH_DIR=$MOOSE_DIR/libmesh
+
+echo $CARDINAL_DIR
+echo $NEKRS_HOME
+echo $PYTHON_PATH
+echo $MOOSE_DIR
+echo $LIBMESH_DIR
+
+export JOBS=16
+export LIBMESH_JOBS=$JOBS
+export MOOSE_JOBS=$JOBS
+
+cd $CARDINAL_DIR
+
+# clean install
+rm *.log
+rm -rf build/ 
+rm -rf install/ 
+rm -rf contrib/*
+
+./scripts/get-dependencies.sh > dep.log
+
+./contrib/moose/scripts/update_and_rebuild_petsc.sh &> petsc.log
+./contrib/moose/scripts/update_and_rebuild_libmesh.sh --enable-xdr-required --with-xdr-include=/usr/include --with-vexcl=no &> libmesh.log
+./contrib/moose/scripts/update_and_rebuild_wasp.sh &> wasp.log
+
+make -j $JOBS OCCA_HIP_ENABLED=$OCCA_HIP_ENABLED  >& opt-make.log
+METHOD=dbg make -j $JOBS OCCA_HIP_ENABLED=$OCCA_HIP_ENABLED >& dbg-make.log
